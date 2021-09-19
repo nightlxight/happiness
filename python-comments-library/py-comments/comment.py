f91c88ebf261633dbdd7a69ab937b9e53cf4ae3a
nightlxight <itsbeadj@gmail.com>
2021-09-19 14:34:12 +0300

    🍖 Add python-comments-library and add file(s) in it :)

---
+++ python-comments-library/py-comments/comment.py
@@ -0,0 +1,5 @@
+# Comments start with #
+
+print("Comments are useful.!") # This is a comment!
+
+x = 10 # Python ignores comments
---
+++ python-comments-library/py-comments/comment2.py
@@ -0,0 +1,4 @@
+"""
+You can add a multiline string (triple quotes) in your code
+Then place your comment in the triple quotes.
+"""
